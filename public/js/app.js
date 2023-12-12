f643ee0c0caedbf0a79d0eb478f86fb2111d33a7
Kingluca4 <luca.gulotta@outlook.it>
2023-12-13 00:21:19 +0100

    updated Weather App

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,35 @@
 console.log('Client side javascript file is loaded!')
+
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+
+
+weatherForm.addEventListener('submit', (e) =>{
+    e.preventDefault()
+
+    const location = search.value
+
+    console.log(location);
+})
+
+
+document.getElementById('weatherInfo').addEventListener('click', async function () {
+    const inputUno = document.getElementById('inputCity').value;
+    const inputDue = document.getElementById('inputRegion').value;
+    
+    const apiUrl = `http://localhost:3000/weather?city=${inputUno}&region=${inputDue}`;
+    
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        console.log('GET request successful');
+        console.log('Response data:', data);
+    } catch (error) {
+        console.error('Error making GET request:', error.message);
+    }
+});
